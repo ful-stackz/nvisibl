01425041e9efddddfe56dc9344e9aac31837bf19
Ivan Stoyanov <ivan.stoyanov@cytosmart.com>
2020-01-23 10:28:36 +0100

    Use single connection string env var instead of constructing at runtime

--- Chatter.Cloud/Helpers/ConnectionStringHelper.cs
+++ Chatter.Cloud/Helpers/ConnectionStringHelper.cs
@@ -5,24 +5,12 @@ namespace Nvisibl.Cloud.Helpers
 {
     internal static class ConnectionStringHelper
     {
-        private const string DBDataSourceEnvVar = "DB_DATASOURCE";
-        private const string DBDatabaseEnvVar = "DB_DATABASE";
+        private const string ConnectionStringEnvVar = "DB_CONNECTIONSTRING";
 
-        public static string GetConnectionString(IConfiguration configuration)
-        {
-            string dataSource = configuration.GetValue(DBDataSourceEnvVar, string.Empty);
-            if (string.IsNullOrEmpty(dataSource))
-            {
-                throw new InvalidOperationException($"{DBDataSourceEnvVar} environment variable missing.");
-            }
-
-            string database = configuration.GetValue(DBDatabaseEnvVar, string.Empty);
-            if (string.IsNullOrEmpty(database))
-            {
-                throw new InvalidOperationException($"{DBDatabaseEnvVar} environment variable missing.");
-            }
-
-            return $"Data Source={dataSource};Initial Catalog={database};Integrated Security=True";
-        }
+        public static string GetConnectionString(IConfiguration configuration) =>
+            configuration.GetValue(ConnectionStringEnvVar, string.Empty) is string connectionString &&
+            !string.IsNullOrEmpty(connectionString)
+            ? connectionString
+            : throw new InvalidOperationException($"{ConnectionStringEnvVar} environment variable missing.");
     }
 }
