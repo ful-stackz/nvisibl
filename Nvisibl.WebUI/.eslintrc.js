3678a79bff3b2c2f5bf9ac59574a3fbf405966b4
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-02-04 23:03:14 +0100

    Fix eslint problems caused by airbnb+typescript config

--- Nvisibl.WebUI/.eslintrc.js
+++ Nvisibl.WebUI/.eslintrc.js
@@ -15,11 +15,19 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: 'module',
     },
+    settings: {
+        'import/resolver': {
+            'node': {
+                'extensions': [ '.js', '.ts' ],
+            },
+        },
+    },
     plugins: [
         '@typescript-eslint',
     ],
     rules: {
         'linebreak-style': 0,
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'indent': [ 'error', 4, { 'SwitchCase': 1 } ],
+        'import/extensions': [ 'error', 'ignorePackages', { 'js': 'never', 'ts': 'never' } ],
     },
 };
