c2e0beecb87e7ab3bab5bb80b59e526109c2cd79
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-02-07 15:46:17 +0100

    Create private chatrooms with an empty name

--- Nvisibl.Business/ChatroomsManager.cs
+++ Nvisibl.Business/ChatroomsManager.cs
@@ -36,9 +36,14 @@ namespace Nvisibl.Business
 
             await EnsureUserExistsAsync(createChatroomModel.OwnerId);
 
+            if (createChatroomModel.IsShared && string.IsNullOrEmpty(createChatroomModel.ChatroomName))
+            {
+                throw new InvalidOperationException($"Shared chatrooms must specify a chatroom name.");
+            }
+
             var chatroom = new Chatroom
             {
-                Name = createChatroomModel.ChatroomName,
+                Name = createChatroomModel.ChatroomName ?? string.Empty,
                 IsShared = createChatroomModel.IsShared,
             };
             await _unitOfWork.GetRepository<IChatroomRepository>().AddAsync(chatroom);
