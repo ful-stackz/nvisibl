3e1abe5cee594bfe62b50433cd8907fb93fb52da
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-02-07 15:44:58 +0100

    Create private chatroom when clicking on a friend with no chatroom

--- Nvisibl.WebUI/src/components/FriendsList.svelte
+++ Nvisibl.WebUI/src/components/FriendsList.svelte
@@ -2,8 +2,10 @@
     import { onMount, onDestroy } from 'svelte';
     import session from '../stores/session';
     import friends from '../stores/friends';
+    import chatrooms from '../stores/chatrooms';
     import Api from '../server/api';
     import User from '../models/user';
+    import Chatroom from '../models/chatroom';
 
     export let api: Api = null;
 
@@ -36,6 +38,47 @@
     onDestroy(() => {
         unsubscribeFriends();
     });
+
+    function handleFriendClick(friend: User): void {
+        const chatroom = chatrooms.get().find((chatroom) => {
+            return chatroom.isShared
+                ? false
+                : chatroom.users.find((user) => user.id === friend.id);
+        });
+        if (chatroom) {
+            // Set as active chatroom
+        } else {
+            createPrivateChatroom(friend)
+                .then((cr: Chatroom) => {
+                    chatrooms.add(cr);
+                    // Set as active chatroom
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }
+
+    async function createPrivateChatroom(friend: User): Promise<Chatroom> {
+        const { user, accessToken } = session.get();
+        const { data } = await api.post(
+            'chatrooms',
+            {
+                ownerId: user.id,
+                isShared: false,
+            },
+            accessToken,
+        );
+        const chatroom = new Chatroom(data.id, data.name, data.isShared, [user, friend]);
+        await api.post(
+            `chatrooms/${chatroom.id}/users`,
+            {
+                userId: friend.id,
+            },
+            accessToken,
+        );
+        return chatroom;
+    }
 </script>
 
 <div class="bg-gray-200 p-3 rounded w-1/4">
@@ -44,7 +87,9 @@
         <div>Loading...</div>
     {:else if visibleFriends.length > 0}
         {#each visibleFriends as friend}
-            <div class="flex flex-row p-3 cursor-pointer rounded hover:bg-gray-300">
+            <div
+                class="flex flex-row p-3 cursor-pointer rounded hover:bg-gray-300"
+                on:click={handleFriendClick(friend)}>
                 <div class="flex-none bg-gray-400 rounded-full w-6 h-6"></div>
                 <div class="flex-shrink truncate ml-2">{friend.username}</div>
             </div>
