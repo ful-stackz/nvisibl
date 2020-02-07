ddbc45a8c9718907d7cd9fbd12a5114b87b80ed4
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-02-07 01:19:00 +0100

    Implement GET users/{id}/chatrooms
    - retrieve the chatrooms a user is part of
    - includes the users part of those chatrooms

---
+++ Nvisibl.Cloud/Models/Responses/UserChatroomsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nvisibl.Cloud.Models.Responses
+{
+    public class UserChatroomsResponse
+    {
+        public IEnumerable<ChatroomResponse> Chatrooms { get; set; } = Array.Empty<ChatroomResponse>();
+    }
+}
