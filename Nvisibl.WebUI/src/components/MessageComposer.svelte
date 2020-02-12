2464a3ef96eba17c9f3ec48096fda5d5a0a7d058
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-02-12 01:07:52 +0100

    Check prop on mount and remove label

--- Nvisibl.WebUI/src/components/MessageComposer.svelte
+++ Nvisibl.WebUI/src/components/MessageComposer.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import SessionManager from '../services/sessionManager';
-    import Message from '../models/message';
 
     export let sessionManager: SessionManager = null;
 
     let value: string = '';
     let placeholder: string = 'Type your message...';
-    $: canSend = !!value;
 
     function sendMessage(): void {
         if (!value) return;
@@ -19,6 +17,10 @@
     function sendMessageOnEnter(e: KeyboardEvent): void {
         if (e.ctrlKey && e.keyCode === 13) sendMessage();
     }
+
+    onMount(() => {
+        if (!sessionManager) throw new Error('SessionManager prop is not provided.');
+    });
 </script>
 
 <div class="h-full bg-white rounded border-gray-200 border-2 overflow-y-auto p-2">
@@ -32,7 +34,7 @@
         </div>
         <div class="flex-none self-center">
             <button
-                class="{canSend ? 'bg-gray-200' : 'bg-transparent text-gray-200 cursor-not-allowed'} border border-gray-200 rounded p-2"
+                class="{!!value ? 'bg-gray-200' : 'bg-transparent text-gray-200 cursor-not-allowed'} border border-gray-200 rounded p-2"
                 on:click={sendMessage}>
                 Send
             </button>
