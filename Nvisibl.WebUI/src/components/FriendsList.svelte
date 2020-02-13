5dcec4f89199c71167b6bb16e445baca75264a75
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-02-13 10:38:22 +0100

    Display friend requests in WebUI

--- Nvisibl.WebUI/src/components/FriendsList.svelte
+++ Nvisibl.WebUI/src/components/FriendsList.svelte
@@ -8,9 +8,23 @@
     export let api: Api = null;
     export let sessionManager: SessionManager = null;
 
+    interface FriendRequest {
+        id: number,
+        sender: {
+            id: number,
+            username: string,
+        },
+        receiver: {
+            id: number,
+            username: string,
+        }
+    }
     let visibleFriends: User[] = [];
     let isLoading: boolean = true;
     let inError: boolean = false;
+    let friendRequests: FriendRequest[] = [];
+    let friendRequestsIsLoading: boolean = true;
+    let friendRequestsInError: boolean = false;
 
     const friendsSub = sessionManager.get().friends.onChange
         .subscribe((current) => visibleFriends = current);
@@ -34,6 +48,16 @@
             .catch((_) => {
                 isLoading = false;
                 inError = true;
+            });
+        api.get(`users/${user.id}/friend-requests`, null, token)
+            .then(({ data }: { data: FriendRequest[] }) => {
+                friendRequests = data.filter((req) => req.receiver.id === user.id);
+                friendRequestsIsLoading = false;
+            })
+            .catch((error) => {
+                console.error(error);
+                friendRequestsIsLoading = false;
+                friendRequestsInError = true;
             });
     });
 
@@ -82,6 +106,12 @@
         );
         return chatroom;
     }
+
+    function answerFriendRequest(request: FriendRequest, accept: boolean): void {
+        const { user, authToken: { token } } = sessionManager.get().auth;
+        api.post(`users/${user.id}/friend-requests/${request.id}`, { accept }, token)
+            .catch(console.error);
+    }
 </script>
 
 <div class="bg-gray-200 p-3 rounded">
@@ -101,5 +131,35 @@
         <div class="text-red-800">Could not load friends</div>
     {:else}
         <div>No friends</div>
+    {/if}
+    <div class="text-lg">Friend requests</div>
+    {#if friendRequestsIsLoading}
+        <div>Loading...</div>
+    {:else if friendRequests.length > 0}
+        {#each friendRequests as request}
+            <div class="flex flex-row p-3 rounded">
+                <div class="flex-grow truncate ml-2">{request.sender.username}</div>
+                <div
+                    class="flex-none rounded-full w-6 h-6 cursor-pointer hover:bg-gray-400 p-1"
+                    on:click={answerFriendRequest(request, true)}
+                >
+                    <svg viewBox="0 0 20 20">
+                        <path fill="current" d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"></path>
+                    </svg>
+                </div>
+                <div
+                    class="flex-none rounded-full w-6 h-6 cursor-pointer hover:bg-gray-400 p-1"
+                    on:click={answerFriendRequest(request, false)}
+                >
+                    <svg viewBox="0 0 20 20">
+                        <path fill="current" d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
+                    </svg>
+                </div>
+            </div>
+        {/each}
+    {:else if friendRequestsInError}
+        <div class="text-red-800">Could not load friends</div>
+    {:else}
+        <div>No requests</div>
     {/if}
 </div>
