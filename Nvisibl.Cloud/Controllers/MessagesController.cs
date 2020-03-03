56838b653dc3518e4b441d91e8793a1f5df56221
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-03-03 19:10:55 +0100

    Implement HTTP endpoint for sending chatroom messages

--- Nvisibl.Cloud/Controllers/MessagesController.cs
+++ Nvisibl.Cloud/Controllers/MessagesController.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 using Nvisibl.Cloud.Models.Requests;
 using Nvisibl.Cloud.Models.Responses;
 using Nvisibl.Business.Models.Messages;
+using Nvisibl.Cloud.Services.Interfaces;
+using Nvisibl.Cloud.Models.Data;
+using Microsoft.AspNetCore.Authorization;
+using Nvisibl.Cloud.Authentication;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Linq;
 
 namespace Nvisibl.Cloud.Controllers
 {
@@ -13,22 +19,44 @@ namespace Nvisibl.Cloud.Controllers
     [ApiController]
     public class MessagesController : ControllerBase
     {
-        private readonly IMessagesManager _messagesManagerService;
+        private readonly IMessagesManager _messagesManager;
+        private readonly INotificationsService _notificationsService;
         private readonly ILogger<MessagesController> _logger;
 
-        public MessagesController(IMessagesManager messagesManagerService, ILogger<MessagesController> logger)
+        public MessagesController(
+            IMessagesManager messagesManagerService,
+            INotificationsService notificationsService,
+            ILogger<MessagesController> logger)
         {
-            _messagesManagerService = messagesManagerService ?? throw new ArgumentNullException(nameof(messagesManagerService));
+            _messagesManager = messagesManagerService ?? throw new ArgumentNullException(nameof(messagesManagerService));
+            _notificationsService = notificationsService ?? throw new ArgumentNullException(nameof(notificationsService));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
+        [Authorize(AuthenticationSchemes = JwtSchemes.Admin + "," + JwtSchemes.User)]
         [HttpPost]
         public async Task<ActionResult> CreateAsync(
-            [FromBody] CreateMessageRequest request)
+            [FromBody] CreateMessageRequest request,
+            [FromServices] IUsersManager usersManager,
+            [FromServices] IChatroomsManager chatroomsManager)
         {
             try
             {
-                var message = await _messagesManagerService.CreateMessageAsync(
+                var sender = await usersManager.GetUserAsync(request.AuthorId);
+                if (sender is null)
+                {
+                    return BadRequest($"User with ID '{request.AuthorId}' does not exist.");
+                }
+
+                var senderChatrooms = await chatroomsManager.GetUserChatroomsAsync(sender);
+                if (senderChatrooms is null || !senderChatrooms.Any(x => x.Id == request.ChatroomId))
+                {
+                    return Unauthorized(
+                        $"User with ID '{request.AuthorId}' is not a member of chatroom with ID " +
+                        $"'${request.ChatroomId}'.");
+                }
+
+                var message = await _messagesManager.CreateMessageAsync(
                     new CreateMessageModel
                     {
                         AuthorId = request.AuthorId,
@@ -36,6 +64,8 @@ namespace Nvisibl.Cloud.Controllers
                         ChatroomId = request.ChatroomId,
                         TimeSentUtc = request.TimeSentUtc,
                     });
+                _notificationsService.EnqueueNotification(new ChatroomMessageNotification(message));
+
                 return new JsonResult(new MessageResponse
                 {
                     AuthorId = message.AuthorId,
