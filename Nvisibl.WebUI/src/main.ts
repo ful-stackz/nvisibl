c7183853b97d47fb813f417ee4f4c9207f6ec7e4
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-03-04 12:33:29 +0100

    Update WS endpoint in the client

--- Nvisibl.WebUI/src/main.ts
+++ Nvisibl.WebUI/src/main.ts
@@ -3,7 +3,7 @@ import Api from './server/api';
 import SessionManager from './services/sessionManager';
 
 const api: Api = new Api('https://nvisibl.azurewebsites.net/api/');
-const webSocketAddress: string = 'wss://nvisibl.azurewebsites.net/ws/chat';
+const webSocketAddress: string = 'wss://nvisibl.azurewebsites.net/ws';
 const sessionManager = new SessionManager(api, webSocketAddress);
 
 const app = new App({
